ddfdc57dc8c39f9e39d9ee0730cd2a5ed680c9f9
dongilc <dongilc@mju.ac.kr>
2021-06-01 21:26:35 +0900

    210601

--- symbolic_modern_robotics.py
+++ symbolic_modern_robotics.py
@@ -19,7 +19,7 @@ def VecToso3(w):
     skew_w = s.Matrix([[0,-o3,o2],
                        [o3,0,-o1],
                        [-o2,o1,0]])
-    return skew_w.evalf()
+    return skew_w
 
 ###### Rotation Matrix
 # Calculate 3-vector from so3
@@ -28,7 +28,7 @@ def so3ToVec(so3mat):
     w[0] = -so3mat[1,2]
     w[1] = so3mat[0,2]
     w[2] = -so3mat[0,1]
-    return w.evalf()
+    return w
 
 ###### Rotation Matrix
 # Calculate unit axis of rotation(omega_hat) and theta from 3-vector
@@ -47,7 +47,7 @@ def MatrixExp3(so3mat):
     skew_w = VecToso3(w_hat)
     I = s.Matrix([[1,0,0],[0,1,0],[0,0,1]])
     R = s.simplify(I + s.sin(theta)*skew_w + (1-s.cos(theta))*skew_w*skew_w)
-    return R.evalf()  
+    return R
         
 ###### Rotation Matrix
 # Calculate so3 from SO3 by Matrix logarithm
@@ -58,7 +58,7 @@ def MatrixLog3(SO3):
     theta = s.simplify(s.acos((tr_R-1)/2))
     w_hat_skew = s.simplify(1/(2*s.sin(theta))*(R-R_T))
     so3 = s.simplify(w_hat_skew*theta)
-    return so3.evalf()
+    return so3
 
 ###### Homogeneous Transform Matrix
 # Calculate se3 from 6-vector twist
@@ -68,7 +68,7 @@ def VecTose3(V):
     skew_w = VecToso3(w)
     se3 = s.Matrix([[skew_w,v],
                     [0,0,0,0]])
-    return se3.evalf()
+    return se3
 
 ###### Homogeneous Transform Matrix
 # Calculate screw-axis and angle from 6-vector
@@ -84,7 +84,7 @@ def AxisAng6(V):
         
     S = s.simplify(V/norm)
     theta_dot = norm
-    return S.evalf(),theta_dot.evalf() 
+    return S,theta_dot 
 
 ###### Homogeneous Transform Matrix
 # Calculate 6-vector twist from se3
@@ -96,7 +96,7 @@ def se3ToVec(se3mat):
     V[3] = se3mat[0,3]
     V[4] = se3mat[1,3]
     V[5] = se3mat[2,3]
-    return V.evalf()
+    return V
 
 ###### Homogeneous Transform Matrix
 # Calculate SE3 from se3 by Matrix exponential
@@ -116,10 +116,10 @@ def MatrixExp6(se3mat):
         R = MatrixExp3(so3_hat*theta)
         I = s.Matrix([[1,0,0],[0,1,0],[0,0,1]])
         G_theta = I*theta + (1-s.cos(theta))*so3_hat + (theta-s.sin(theta))*so3_hat*so3_hat
-        p = s.simplify(G_theta*v).evalf()
+        p = s.simplify(G_theta*v)
         SE3 = s.Matrix([[R,p],
                         [0,0,0,1]])
-    #return V,w,w_hat,theta,v,so3,R,p,SE3
+    #return V,w,w_hat,theta,v,so3_hat,R,p,SE3
     return SE3
 
 ###### Homogeneous Transform Matrix
@@ -162,3 +162,61 @@ def FKinBody(M, Blist, thetalist):
     for i in range(len(thetalist)):
        T = s.simplify(T@MatrixExp6(VecTose3(Blist[:, i]*thetalist[i])))
     return T
+
+##### Adjoint of T
+# Computes Adjoint of T
+def Adjoint(T):
+    R = T[0:3,0:3]
+    p = T[0:3,3]
+    
+    Ad_T = s.Matrix([[R, s.Matrix.zeros(3)],
+                     [VecToso3(p)@R ,R]])
+    return Ad_T
+
+def TransInv(T):
+    R = T[0:3,0:3]
+    p = T[0:3,3]
+
+    T_inv = s.Matrix([[R.T, -R.T@p],
+                      [0,0,0,1]])
+    return T_inv
+
+###### Body Jacobian
+# Computes Body Jacobian
+def JacobianBody(Blist, thetalist):
+    Jb = Blist.copy()
+    T = s.Matrix.eye(4)
+    for i in range(len(thetalist) - 2, -1, -1):
+        T = s.simplify( T @ MatrixExp6(VecTose3(Blist[:, i+1]*-thetalist[i+1])) )
+        Jb[:, i] = s.simplify(Adjoint(T)@Blist[:, i])
+    return Jb
+
+###### Space Jacobian
+# Computes Space Jacobian
+def JacobianSpace(Slist, thetalist):
+    Js = Slist.copy()
+    T = s.Matrix.eye(4)
+    for i in range(1, len(thetalist)):
+        T = s.simplify( T @ MatrixExp6(VecTose3(Slist[:, i-1]*thetalist[i-1])) )
+        Js[:, i] = s.simplify(Adjoint(T)@Slist[:, i])
+        #print(i, Js[:, i])
+    return Js
+
+###### Numerical Inverse Kinematics using Newton-Rapton Method
+# Computes inverse kinematics in the body frame
+def IKinBody(Blist, M, Tsd, thetalist0, eomg, ev):
+    thetalist = thetalist0
+    i = 0
+    maxiterations = 20
+    T_sb = FKinBody(M, Blist, thetalist)
+    T_sb_inv = TransInv(T_sb)
+    T_bd = s.simplify(T_sb_inv@Tsd)
+    Vb = se3ToVec(MatrixLog6(T_bd))
+    #err = np.linalg.norm([Vb[0], Vb[1], Vb[2]]) > eomg or np.linalg.norm([Vb[3], Vb[4], Vb[5]]) > ev
+    #while err and i < maxiterations:
+    #    thetalist = thetalist + np.dot(np.linalg.pinv(JacobianBody(Blist, thetalist)), Vb)
+    #    i = i + 1
+    #    Vb = se3ToVec(MatrixLog6(np.dot(TransInv(FKinBody(M, Blist, thetalist)), T)))
+    #    err = np.linalg.norm([Vb[0], Vb[1], Vb[2]]) > eomg or np.linalg.norm([Vb[3], Vb[4], Vb[5]]) > ev
+    #return (thetalist, not err)
+    return Vb
